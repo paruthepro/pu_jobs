1e91a7cc139c5b9e376af7a0df1bb131e9e81b86
Torch ICO <tommyrpuk@gmail.com>
2024-03-21 16:33:44 +0000

    Fixed useables (again)

--- pu_mechanic/server/main.lua
+++ pu_mechanic/server/main.lua
@@ -19,149 +19,149 @@ RegisterNetEvent('pu_jobs:server:finish', function(item, mods)
 end)
 
 -- Items
-exports.qbx_core:CreateUseableItem("engine_level1", function(source, item)
+exports.qbx_core:CreateUseableItem("engine_level1", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('engine_level1') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'engine_level1') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', 0)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("engine_level2", function(source, item)
+exports.qbx_core:CreateUseableItem("engine_level2", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('engine_level2') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'engine_level2') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("engine_level3", function(source, item)
+exports.qbx_core:CreateUseableItem("engine_level3", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('engine_level3') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', item.name, 2)
+	if exports.ox_inventory:Search(Player, 'count', 'engine_level3') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', 2)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("engine_level4", function(source, item)
+exports.qbx_core:CreateUseableItem("engine_level4", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('engine_level4') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', item.name, 3)
+	if exports.ox_inventory:Search(Player, 'count', 'engine_level4') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'engine', 3)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("trans_level1", function(source, item)
+exports.qbx_core:CreateUseableItem("trans_level1", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('trans_level1') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'trans_level1') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', 0)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("trans_level2", function(source, item)
+exports.qbx_core:CreateUseableItem("trans_level2", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('trans_level2') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'trans_level2') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("trans_level3", function(source, item)
+exports.qbx_core:CreateUseableItem("trans_level3", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('trans_level3') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', item.name, 2)
+	if exports.ox_inventory:Search(Player, 'count', 'trans_level3') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'transmission', 2)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("suspension_level1", function(source, item)
+exports.qbx_core:CreateUseableItem("suspension_level1", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('suspension_level1') ~= nil then
-        TriggerClientEvent('pu_jobs:client:useitem', source, 'suspension', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'suspension_level1') ~= nil then
+        TriggerClientEvent('pu_jobs:client:useitem', source, 'suspension', 0)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("suspension_level2", function(source, item)
+exports.qbx_core:CreateUseableItem("suspension_level2", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('suspension_level2') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'suspension_level2') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("suspension_level3", function(source, item)
+exports.qbx_core:CreateUseableItem("suspension_level3", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('suspension_level3') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', item.name, 2)
+	if exports.ox_inventory:Search(Player, 'count', 'suspension_level3') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', 2)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("suspension_level4", function(source, item)
+exports.qbx_core:CreateUseableItem("suspension_level4", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('suspension_level4') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', item.name, 3)
+	if exports.ox_inventory:Search(Player, 'count', 'suspension_level4') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'suspension', 3)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("brakes_level1", function(source, item)
+exports.qbx_core:CreateUseableItem("brakes_level1", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('brakes_level1') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'brakes_level1') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', 0)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("brakes_level2", function(source, item)
+exports.qbx_core:CreateUseableItem("brakes_level2", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('brakes_level2') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'brakes_level2') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("brakes_level3", function(source, item)
+exports.qbx_core:CreateUseableItem("brakes_level3", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('brakes_level3') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', item.name, 2)
+	if exports.ox_inventory:Search(Player, 'count', 'brakes_level3') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'brakes', 2)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("turbo", function(source, item)
+exports.qbx_core:CreateUseableItem("turbo", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('turbo') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'turbo', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'turbo') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'turbo', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("armor_level1", function(source, item)
+exports.qbx_core:CreateUseableItem("armor_level1", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('armor_level1') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'armor_level1') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', 0)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("armor_level2", function(source, item)
+exports.qbx_core:CreateUseableItem("armor_level2", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('armor_level2') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', item.name, 1)
+	if exports.ox_inventory:Search(Player, 'count', 'armor_level2') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', 1)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("armor_level3", function(source, item)
+exports.qbx_core:CreateUseableItem("armor_level3", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('armor_level3') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', item.name, 2)
+	if exports.ox_inventory:Search(Player, 'count', 'armor_level3') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', 2)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("armor_level4", function(source, item)
+exports.qbx_core:CreateUseableItem("armor_level4", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('armor_level4') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', item.name, 3)
+	if exports.ox_inventory:Search(Player, 'count', 'armor_level4') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'armor', 3)
     end
 end)
 
-exports.qbx_core:CreateUseableItem("car_downgrade", function(source, item)
+exports.qbx_core:CreateUseableItem("car_downgrade", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('car_downgrade') ~= nil then
-        TriggerClientEvent("pu_jobs:client:useitem", source, 'downgrade', item.name, 0)
+	if exports.ox_inventory:Search(Player, 'count', 'car_downgrade') ~= nil then
+        TriggerClientEvent("pu_jobs:client:useitem", source, 'downgrade', 0)
     end
 end)
 
 exports.qbx_core:CreateUseableItem("body_kit", function(source)
     local Player = exports.qbx_core:GetPlayer(source)
-	if Player.Functions.GetItemByName('body_kit') ~= nil then
+	if exports.ox_inventory:Search(Player, 'count', 'body_kit') ~= nil then
         local kit = exports.ox_inventory:Search(source, 1, 'body_kit')
         exports.ox_inventory:SetDurability(source, kit.slot, 100)
         TriggerClientEvent("pu_jobs:client:body_repair", source)
